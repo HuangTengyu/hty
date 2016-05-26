25f7ece658bc9831b80564f810f44fe2b5adfb49
HuangTengyu <1445763458@qq.com>
2016-05-26 10:32:21 +0800

    提交本地项目-hty -01

---
+++ test_git/src/com/meiyue/string/StringTest.java
@@ -0,0 +1,10 @@
+package com.meiyue.string;
+
+/**
+ * Created by Tengyu on 2016/5/26.
+ */
+public class StringTest {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
